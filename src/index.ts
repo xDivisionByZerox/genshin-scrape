362f17db316108fffd8922fb1abb199ac6f9d212
xDivisionByZerox <leyla.jaehnig@gmx.de>
2023-05-25 19:22:05 +0200

    feat: entry script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-import { BooksScraper } from './scripts/books';
+import { BaseScraper } from './scripts/base.scraper';
+import { BookStrategy } from './scripts/strategies/book.strategy';
+import { IScrapingStrategy } from './scripts/strategies/scraping-strategy.interface';
+import { WeaponStrategy } from './scripts/strategies/weapon.strategy';
+import { DataType } from './util/write-data';
 
 (async () => {
-  await new BooksScraper().scrapeData();
+  const options: {
+    strategy: IScrapingStrategy<unknown>,
+    type: DataType,
+  }[] = [
+      { strategy: new BookStrategy(), type: 'books' },
+      { strategy: new WeaponStrategy(), type: 'weapons' },
+    ];
+
+  for (const { strategy, type } of options) {
+    const scraper = new BaseScraper(strategy, type);
+    await scraper.scape().catch(
+      (error) => console.error(`Failed scraping process for resource [${type}]. Cause:\n${error}`),
+    );
+  }
 })()
